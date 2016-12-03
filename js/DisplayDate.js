663eb8eee7357dfcc5ffbe6ca3f2191bd70743da
hoq78 <tahmidhoque@gmail.com>
2016-12-03 12:56:06 +0000

    The dashboard page is almost done
    the calendar widget is the only object left to create. there was as bug with the analogue clock which has now been fixed.

---
+++ js/DisplayDate.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    getDate();
+}, false);
+
+function getDate() {
+    var date = new Date();
+    var daynumber = date.getDay();
+    var dateOfMonth = date.getDate();
+    var month = date.getMonth();
+    var year = date.getFullYear();
+    var daysInTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var monthsInYear = ["February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "January"];
+    var day;
+
+    day = daysInTheWeek[daynumber];
+    month = monthsInYear[month - 1];
+    let dateOut = day + " " + dateOfMonth + " " + month + " " + year;
+    $("#date").append(dateOut);
+    //document.getElementById('date').innerHTML = day + ":" + daynumber + ":" + month+ ":" + year ;
+
+}
